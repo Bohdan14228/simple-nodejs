2186cb9dfdbc7e5509133aa6eba23c181152b6db
Bohdan14228 <124456152+Bohdan14228@users.noreply.github.com>
2025-12-06 18:33:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 3000; // Используем порт 3000
+const port = 80; // Используем порт 80
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
